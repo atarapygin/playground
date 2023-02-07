63bfe2b198f4e3aeadcfc30a9f000b9bb4ef238a
Tarapygin Aleksey <atarapugin@gmail.com>
2023-02-07 17:23:03 +0100

    async thunk actions was implemented

--- redux-toolkit-todo-app/src/App.jsx
+++ redux-toolkit-todo-app/src/App.jsx
@@ -1,32 +1,38 @@
-import {useState} from 'react';
-import { useDispatch } from 'react-redux';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { addTodo } from './store/todoSlice';
-import NewTodoForm from './components/NewTodoForm';
-import TodoList from './components/TodoList';
+import { addNewTodo, fetchTodos } from "./store/todoSlice";
 
-import './App.css';
+import NewTodoForm from "./components/NewTodoForm";
+import TodoList from "./components/TodoList";
 
+import "./App.css";
 
 function App() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
+  const { status, error } = useSelector(state => state.todos);
   const dispatch = useDispatch();
 
   const handleAction = () => {
-    if(text.trim().length) {
-      dispatch(addTodo({text}));
-      setText('');
+    if (text.trim().length) {
+      dispatch(addNewTodo( text ));
+      setText("");
     }
-  }
+  };
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
 
   return (
-    <div className='App'>
-      <NewTodoForm
-        value={text}
-        updateText={setText}
-        handleAction={handleAction}
-      />
-      <TodoList />
+    <div className="App">
+      {status === 'loading' && <h2>Loading...</h2> }
+        <NewTodoForm
+          value={text}
+          updateText={setText}
+          handleAction={handleAction}
+        />
+      {error ? <h2>An error occured: {error}</h2> : <TodoList />}
     </div>
   );
 }
