6795539a7cdc5305ee79b4bef0ad113b2965f50e
Tarapygin Aleksey <atarapugin@gmail.com>
2023-02-27 12:35:09 +0100

    Weather app

--- weather-nodejs-app/public/js/scripts.js
+++ weather-nodejs-app/public/js/scripts.js
@@ -1,1 +1,26 @@
-console.log("CLient side javascript file is locaded");
+const weatherForm = document.querySelector("form");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const locationSearchValue = document.querySelector("input").value;
+
+  const messageOne = document.querySelector("#message-1");
+  const messageTwo = document.querySelector("#message-2");
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch(`http://localhost:3000/weather?address=${locationSearchValue}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = "Error: " + data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
+});
---
+++ weather-nodejs-app/src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1Ijoib25ub2N5bSIsImEiOiJjbGVpZGZqeDQwMGNpM3pucG81ajJleDZuIn0.JRUt74eJ_ngxeZuB8JeLqQ&limit=1";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
