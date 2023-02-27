9fde2434cb5288ad59dabe28c167c2997448ad77
Tarapygin Aleksey <atarapugin@gmail.com>
2023-02-27 13:15:28 +0100

    Heroku deploy files preparation

--- weather-nodejs-app/public/js/scripts.js
+++ weather-nodejs-app/public/js/scripts.js
@@ -11,16 +11,14 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:3000/weather?address=${locationSearchValue}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = "Error: " + data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${locationSearchValue}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = "Error: " + data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
