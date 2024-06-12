425315716f6a7f5d9c18a084cd42760bf22f72a6
Tarapygin Oleksii <atarapugin@gmail.com>
2024-06-12 16:00:47 +0200

    Another minor app was added

---
+++ nextjs-14-apps/router-staff/app/(content)/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFound() {
+  return (
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>Unfortunatelly, we could not find requested article!</p>
+    </div>
+  );
+}
